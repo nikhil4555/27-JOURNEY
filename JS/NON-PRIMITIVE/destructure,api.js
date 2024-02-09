9f129d087103d4173f72f409b9b4a74b23495371
nikhil4555 <nikhil.20bce7200@vitap.ac.in>
2024-02-09 14:40:31 +0530

    destructure

---
+++ JS/NON-PRIMITIVE/destructure,api.js
@@ -0,0 +1,11 @@
+const course = {
+    name: "Maths in english",
+    price:9999,
+    Instructor:"Nikhil"
+}
+course.Instructor
+const {Instructor: instr}=course;//destructering
+console.log(instr);//mainly used in react 
+
+//************API */-->in array formaat also present
+//json {  }
