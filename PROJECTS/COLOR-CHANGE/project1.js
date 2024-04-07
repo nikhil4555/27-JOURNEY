6f01823c943285befab6e249261ce494645e7b0d
nikhil4555 <nikhil.20bce7200@vitap.ac.in>
2024-04-07 11:06:32 +0530

    commit

--- PROJECTS/COLOR-CHANGE/project1.js
+++ PROJECTS/COLOR-CHANGE/project1.js
@@ -2,7 +2,7 @@ const boxes = document.querySelectorAll(".box");
 
 const body = document.querySelector("body");
 
-boxes.forEach(function(box) {
+boxes.forEach((box)=> {
     box.addEventListener('click', function(e) {
         body.style.backgroundColor = e.target.id;
     }); // Closing parenthesis was missing here
