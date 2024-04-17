478eabf58074112f321ea9cc34abffa805dc9f72
nikhil4555 <nikhil.20bce7200@vitap.ac.in>
2024-04-17 12:13:27 +0530

    promise_1

--- PROMISES/promise_1.js
+++ PROMISES/promise_1.js
@@ -123,7 +123,7 @@ async function getAallUsers(){
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users')
         // console.log(response);
-        const data = await response.json()
+        const data = aw ait response.json()
         console.log(data);
     } catch (error) {
         console.log("E: ",error);
